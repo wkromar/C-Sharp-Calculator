7bd00f53c50f743fccb5f52662ed3448f628dba0
Woody Kromar <wkromar12@gmail.com>
2021-04-08 14:00:45 -0500

    calculator functions completed

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C__Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // to start, run dotnet new console in terminal
+            // run dotnet run to run terminal program
+
+
+            // basic addition by converting a string into an integer
+
+            // int num = Convert.ToInt32("45");
+            // Console.WriteLine(num + 6);
+            // Console.ReadLine();
+
+            // Console.Write("Enter a number: ");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+            //  Console.Write("Enter another number: ");
+            // int num2 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(num1 + num2);
+            // Console.ReadLine();
+
+            // now we can also work with decimals
+
+            // Console.Write("Enter a number: ");
+            // double num1 = Convert.ToDouble(Console.ReadLine());
+            //  Console.Write("Enter another number: ");
+            // double num2 = Convert.ToDouble(Console.ReadLine());
+            // Console.WriteLine(num1 + num2);
+            // Console.ReadLine();
+        }
+    }
+}
